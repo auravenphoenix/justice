7ff60237a68266f701778241f45fa81303fad517
Aymeric Pellé <aympelle@gmail.com>
2023-03-11 16:34:35 +0100

    M auto_commit_push.py

--- auto_commit_push.py
+++ auto_commit_push.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import git.config
 from git import Repo
 
@@ -6,7 +8,7 @@ class AutoCommitPush:
     def __init__(self, repository_path):
         print(f"INFO - Load git repository.")
         self.__repository = Repo(repository_path)
-        self.__git : git.Git = self.__repository.git
+        self.__git: git.Git = self.__repository.git
 
     def commit_and_push_all(self):
         for item in self.__repository.index.diff(None):
@@ -14,10 +16,14 @@ class AutoCommitPush:
         for item in self.__repository.untracked_files:
             self.commit_and_push_file(item)
 
-    def commit_and_push_file(self, file, change_type = 'A'):
+    def commit_and_push_file(self, file, change_type='A'):
         commit_msg = f"{change_type} {file}"
         print(f"### {commit_msg}")
-        self.__repository.index.add([file])
+        match change_type:
+            case 'D':
+                self.__repository.index.remove([file])
+            case _:
+                self.__repository.index.add([file])
         self.__repository.index.commit(commit_msg)
         self.__repository.remote().push()
 
