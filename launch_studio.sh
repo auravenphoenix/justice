bf6c058b90996b9c5336fd54a271b1a4498ca501
Aymeric Pellé <aympelle@gmail.com>
2023-06-05 17:04:05 +0200

    M launch_studio.sh

--- launch_studio.sh
+++ launch_studio.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-(obs-studio > /dev/null 2> /dev/null &)
+(/bin/obs-studio > /dev/null 2> /dev/null &)
 (code .&)
 xdg-open ~/auravenphoenix_justice/attaque 
 xdg-open ~/auravenphoenix_justice/defense 
